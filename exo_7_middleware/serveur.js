00a7bf79cf77687dd6cdd375bc9ac84313af5465
wassim-slimane <wassim.slimane01@gmail.com>
2022-04-29 16:46:55 +0200

    refacto(serveur): middleware timer

--- exo_7_middleware/serveur.js
+++ exo_7_middleware/serveur.js
@@ -1,28 +1,23 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
+  let start = performance.now();
   try {
-   
     if (req.url === "/") {
-	var start = new Date();
-	var finish = new Date();
-	var difference = new Date();
-	difference.setTime(finish.getTime() - start.getTime());
-	console.log( difference.getMilliseconds() );
       res.writeHead(200, {'content-type':'text/html'});
       res.write('<h1>HOMEPAGE</h1>');
-      res.end();
     } else {
       res.writeHead(404, {'content-type':'text/html'});
       res.write('<h1>404 Not Found</h1>');
-      res.end()
     }
   } catch (err) {
     res.writeHead(500, {'content-type':'text/html'});
     res.write('<h1>500 Internal Server Error</h1>');
-    res.end()
   }
-} );
+  res.end();
+  let end = performance.now();
+  console.log("requête a pris", end - start);
+});
 
 server.listen(3000);
 console.log('Server écoute sur le port 3000');
